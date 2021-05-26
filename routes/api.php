dae9c7e75fe76d1f40e75ec7cd8e322204c4af44
nhan2804 <akainu345@gmail.com>
2021-05-26 09:16:51 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TodoController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::resource('todo', TodoController::class);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
