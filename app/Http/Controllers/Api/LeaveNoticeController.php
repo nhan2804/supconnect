0426627bffc5beea12268afd272f1f583128c20c
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2021-06-07 00:42:48 +0700

    add leave notice api

--- app/Http/Controllers/Api/LeaveNoticeController.php
+++ app/Http/Controllers/Api/LeaveNoticeController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use App\Models\Assignment;
+use App\Models\LeaveNotice;
 use App\Models\Student_Of_Subject_Class;
+use App\Models\Subject_Class;
 use App\Models\Subject_List;
+use App\Models\Lecturer;
+use App\Models\Lecturer_Degree_Type;
 use SebastianBergmann\Environment\Console;
 
-class AssignmentController extends Controller
+class LeaveNoticeController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -42,37 +45,33 @@ class AssignmentController extends Controller
     public function show($id)
     {
 
-        $assignment = Assignment::where('assignment_id', $id)->get();
+        $leavenotice = LeaveNotice::where('leave_notice_id', $id)->get();
         return response()->json([
             'success' => true,
-            'assignments' => $assignment
+            'leavenotices' => $leavenotice
         ]);
     }
 
-    public function studentAssignment($student_id)
+    public function studentLeaveNotice($student_id)
     {
         $now = date('Y-m-d H:i:s');
-        $assignments = Assignment::join('student_of_subject_class', 'assignment.subject_class', 'student_of_subject_class.subject_class')
-            ->join('subject_class', 'subject_class.subject_class_id', 'assignment.subject_class')
+        $leavenotices = LeaveNotice::join('student_of_subject_class', 'leave_notice.subject_class', 'student_of_subject_class.subject_class')
+            ->join('subject_class', 'subject_class.subject_class_id', 'leave_notice.subject_class')
             ->where('student_of_subject_class.student_id', $student_id)
             ->where('subject_class.date_start', '<=', $now)
             ->where('subject_class.date_end', '>=', $now)
-            ->select('assignment.*', 'subject_class.subject_class_name as class_name')
+            ->select('leave_notice.*', 'subject_class.lecturer_id as lecturer_id')
             ->get();
-
-        $result = (array) Arr::sort($assignments, function ($assignment) {
-            return strtotime($assignment->deadline) - strtotime(date('Y-m-d H:i:s'));
-        });
-
-        $arr = [];
-
-        foreach ($result as $item) {
-            array_push($arr, $item);
+        foreach ($leavenotices as $leavenotice) {
+            $lecturer = Lecturer::find($leavenotice->lecturer_id);
+            $lecturer_degree = Lecturer_Degree_Type::find($lecturer->degree);
+            $leavenotice->lecturer = $lecturer_degree->abbreviation . ' ' . $lecturer->first_name_lecturer . ' ' . $lecturer->last_name_lecturer;
         }
+
 
         return response()->json([
             'success' => true,
-            'assignments' => $arr
+            'leavenotices' => $leavenotices
         ]);
     }
 
